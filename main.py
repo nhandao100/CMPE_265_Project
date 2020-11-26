64ed5dbc13f9620d998b71664958507a3bc8e652
Ray Ma <32551600+cmpe130weifeng@users.noreply.github.com>
2020-11-26 12:21:32 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import random
+import sys
+
+from pip._vendor.distlib.compat import raw_input
+
+human_score = 0
+computer_score = 0
+
+def generate_random_choice():
+    computer_choice = random.choice(["R", "P", "S"]) # create a random choice
+    print("Computer's choice is", computer_choice)
+    return computer_choice
+
+def prompt_choice():
+    my_choice = raw_input("Enter you choice (R/P/S),"
+                          "Or hit ENTER to quit: ").upper()
+    if len(my_choice) > 1:
+        print ("Error! Only 1 characters allowed!")
+        sys.exit()
+    if not my_choice:
+        print("You quit the game.")
+        sys.exit()
+    if (my_choice == "R") or (my_choice=="S") or (my_choice=="P"):
+        return my_choice
+    else:
+        print("Must be R/S/P!")
+        sys.exit()
+
+def compete_board(user_choice, AI_choice):
+    global human_score
+    global computer_score
+    if(user_choice == "R" and AI_choice == "S"):
+        human_score = human_score + 1
+    elif(user_choice == "S" and AI_choice == "P"):
+        human_score = human_score + 1
+    elif (user_choice == "P" and AI_choice == "R"):
+        human_score = human_score + 1
+    elif(user_choice == AI_choice):
+        pass
+    else:
+        computer_score = computer_score + 1
+
+def main():
+    while(1):
+        user_choice = prompt_choice()
+        computer_choice = generate_random_choice()
+        compete_board(user_choice, computer_choice)
+
+        print("Human score is", human_score)
+        print("Computer score is", computer_score)
+
+main()
